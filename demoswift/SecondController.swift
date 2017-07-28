58fc8f10bc926b8a43ce34b331d077d2fb475004
miaomiaotutu <568846806@qq.com>
2017-07-28 17:31:48 +0800

    dfdgf

--- demoswift/SecondController.swift
+++ demoswift/SecondController.swift
@@ -16,24 +16,8 @@ class SecondController: UIViewController {
 
         self.title = "我的"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("hahahha")
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
